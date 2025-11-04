23394ca5736dd9d2c3dbe1bbfa4c7c02b56c0c16
cutecchiavalentino <cutecchiavalentino@gmail.com>
2025-11-04 12:20:22 +0100

    aggiunto js

---
+++ backend/prova.js
@@ -0,0 +1,17 @@
+/*parte Vale
+name, model, length, manufacturer, crew, passengers, vehicle_class, films
+
+
+*/
+vehicles = {
+    "name" : "https://swapi.dev/api/vehicles/name", 
+    "model" : "https://swapi.dev/api/vehicles/model",
+    "length" : "https://swapi.dev/api/vehicles/length",
+    "manufacturer" : "https://swapi.dev/api/vehicles/manufacturer",
+    "crew" : "https://swapi.dev/api/vehicles/crew",
+    "passengers" : "https://swapi.dev/api/vehicles/passengers",
+    "vehicle_class" : "https://swapi.dev/api/vehicles/vehicle_class",
+    "films" : {
+                "name" : "https://swapi.dev/api/vehicles/films/name"
+              }
+}
