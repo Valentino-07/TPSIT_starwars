909624529e99069b91e93dddd1dd72845a35a08b
cutecchiavalentino <cutecchiavalentino@gmail.com>
2025-11-07 13:21:18 +0100

    TEDUA

--- backend/prova.js
+++ backend/prova.js
@@ -7,4 +7,21 @@ fetch("https://swapi.dev/api/vehicles/")
 .then(data => {
     console.log(data.results); // array di veicoli
   })
+
+
+
+document.getElementById("btn1").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn2").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn3").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn4").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn5").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn6").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn7").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn8").addEventListener("click", () => mostraDettagliVeicolo(""));
+
+
+
+
+function mostraDettagliVeicolo(nome_veicolo){
   
+}
