3c55dc4dadc559bda64f5a7c0053e03fffae7a39
cutecchiavalentino <cutecchiavalentino@gmail.com>
2025-11-11 11:56:00 +0100

    ..

--- backend/prova.js
+++ backend/prova.js
@@ -8,20 +8,16 @@ fetch("https://swapi.dev/api/vehicles/")
     console.log(data.results); // array di veicoli
   })
 
-
-
-document.getElementById("btn1").addEventListener("click", () => mostraDettagliVeicolo(""));
-document.getElementById("btn2").addEventListener("click", () => mostraDettagliVeicolo(""));
-document.getElementById("btn3").addEventListener("click", () => mostraDettagliVeicolo(""));
-document.getElementById("btn4").addEventListener("click", () => mostraDettagliVeicolo(""));
-document.getElementById("btn5").addEventListener("click", () => mostraDettagliVeicolo(""));
+document.getElementById("btn1").addEventListener("click", () => mostraDettagliVeicolo("AT-AT"));
+document.getElementById("btn2").addEventListener("click", () => mostraDettagliVeicolo("AT-ST"));
+document.getElementById("btn3").addEventListener("click", () => mostraDettagliVeicolo("Sand Crawler"));
+document.getElementById("btn4").addEventListener("click", () => mostraDettagliVeicolo("T-16 skyhopper"));
+document.getElementById("btn5").addEventListener("click", () => mostraDettagliVeicolo("Snowspeeder"));
 document.getElementById("btn6").addEventListener("click", () => mostraDettagliVeicolo(""));
 document.getElementById("btn7").addEventListener("click", () => mostraDettagliVeicolo(""));
 document.getElementById("btn8").addEventListener("click", () => mostraDettagliVeicolo(""));
 
-
-
-
 function mostraDettagliVeicolo(nome_veicolo){
-  
+
 }
+//dobbiamo avere i dati del veicolo per mandarli a "dettagli.html"
