df07eae74dde48507944989d0ad46b2aabd72633
Ale-Delu <alesssandrodl0110@gmail.com>
2025-11-11 22:39:10 +0100

    voglio creare una stringa dal data ritornato da swapi

--- backend/prova.js
+++ backend/prova.js
@@ -17,7 +17,30 @@ document.getElementById("btn6").addEventListener("click", () => mostraDettagliVe
 document.getElementById("btn7").addEventListener("click", () => mostraDettagliVeicolo(""));
 document.getElementById("btn8").addEventListener("click", () => mostraDettagliVeicolo(""));
 
+let stringa="";
+
 function mostraDettagliVeicolo(nome_veicolo){
 
 }
 //dobbiamo avere i dati del veicolo per mandarli a "dettagli.html"
+
+<script>
+  $(document).ready(function () {
+    $.get("https://swapi.dev/api/vehicles/?page=1", function(data){
+      stringa+="<table>";
+      stringa+="<tr>";
+      stringa+="<th>name<\/th>";
+      stringa+="<th>model<\/th>";
+      stringa+="<th>length<\/th>";
+      stringa+="<th>manufacturer<\/th>";
+      stringa+="<th>crew<\/th>";
+      stringa+="<th>passengers<\/th>";
+      stringa+="<th>vehicle_class<\/th>";
+      stringa+="<th>films<\/th>";
+      for(let i=0; i<results; i++){
+        
+      }
+    }
+    )
+  });
+</script>
